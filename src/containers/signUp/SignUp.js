bdb8baf61175d964ed9af5d72da4410188d1e37c
PatOn <mahomed2278@gmail.com>
2021-04-25 17:07:19 +0100

    good memory but short memory - deleted comment

--- src/containers/signUp/SignUp.js
+++ src/containers/signUp/SignUp.js
@@ -4,8 +4,6 @@ import SignUpForm from "../../components/signUpForm/SignUpForm";
 import ConfirmationFormPage from "../../components/confirmationFormPage/ConfirmationFormPage";
 import CompletionFormScreen from "../../components/completionFormScreen/CompletionFormScreen";
 
-// import classes from "./SignUp.module.css";
-
 import { checkValidity } from "../../utility/utility";
 
 const FIRST_FORM_PAGE = 0;
